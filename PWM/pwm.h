aa2f40ef24978450a699ca363059516fda7d54e8
SRS <63800307+SARS-CLASSIFY@users.noreply.github.com>
2021-02-07 18:42:55 +0800

    Update pwm.h

--- PWM/pwm.h
+++ PWM/pwm.h
@@ -1,10 +1,6 @@
 #ifndef _TIMER_H
 #define _TIMER_H
-#include "sys.h"
-//////////////////////////////////////////////////////////////////////////////////	 
-//STM32F4工程模板-库函数版本
-//淘宝店铺：http://mcudev.taobao.com								  
-////////////////////////////////////////////////////////////////////////////////// 	
+#include "sys.h"	
 
 void TIM3_PWM_Init(u32 arr,u32 psc);
 void set_pulse(TIM_TypeDef* TIMx,u32 arr,u32 pulse,u8 channel);
